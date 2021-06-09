ba5265a57ea77136da82b29828d82d7dcf6c32fd
Boli-0603 <bli0603@gmail.com>
2021-06-09 12:33:36 +1200

    End of section 13

--- client/src/app/core/section-header/section-header.component.ts
+++ client/src/app/core/section-header/section-header.component.ts
@@ -8,7 +8,7 @@ import { BreadcrumbService } from 'xng-breadcrumb';
   styleUrls: ['./section-header.component.scss']
 })
 export class SectionHeaderComponent implements OnInit {
-  breadcrumb$: Observable<any[]>;
+  breadcrumb$: Observable<any[]> | undefined;
   
   constructor(private bcService: BreadcrumbService) { }
 
