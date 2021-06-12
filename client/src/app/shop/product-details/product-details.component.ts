bd25988b2e64805ffa18b01a0ee7757622694df6
Boli-0603 <bli0603@gmail.com>
2021-06-12 15:48:33 +1200

    End of section 14

--- client/src/app/shop/product-details/product-details.component.ts
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { IProduct } from 'src/app/shared/models/product';
-import { BreadcrumbService } from 'xng-breadcrumb';
 import { ShopService } from '../shop.service';
+import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
+import { BasketService } from 'src/app/basket/basket.service';
 
 @Component({
   selector: 'app-product-details',
@@ -10,20 +11,39 @@ import { ShopService } from '../shop.service';
   styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent implements OnInit {
-  product: IProduct;
+  product!: IProduct;
+  quantity = 1;
 
-  constructor(private ShopService: ShopService, private activateRoute: ActivatedRoute, private bcService: BreadcrumbService) {
-    this.bcService.set('@productDetails', ' ');
+  constructor(private shopService: ShopService,
+              private activateRoute: ActivatedRoute,
+              private bcService: BreadcrumbService,
+              private basketService: BasketService) {
+    this.bcService.set('@productDetails', '');
   }
+
   ngOnInit() {
     this.loadProduct();
   }
 
+  addItemToBasket() {
+    this.basketService.addItemToBasket(this.product, this.quantity);
+  }
+
+  incrementQuantity() {
+    this.quantity++;
+  }
+
+  decrementQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
+  }
+
   loadProduct() {
-    this.ShopService.getProduct(+this.activateRoute.snapshot.paramMap.get('id')).subscribe(product =>{
+    this.shopService.getProduct(+this.activateRoute.snapshot.paramMap.get('id')).subscribe(product => {
       this.product = product;
       this.bcService.set('@productDetails', product.name);
-    }, error=> {
+    }, error => {
       console.log(error);
     });
   }
