5d9e0b44f54f9c53f7915d41bf4728f9033af32e
Boli-0603 <bli0603@gmail.com>
2021-06-16 12:29:32 +1200

    End of section 17

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -6,8 +6,9 @@ import { TestErrorComponent } from './test-error/test-error.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import {ToastrModule} from 'ngx-toastr';
-import { SectionHeaderComponent } from './section-header/section-header.component';
 import {BreadcrumbModule} from 'xng-breadcrumb';
+import { SectionHeaderComponent } from './section-header/section-header.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -17,12 +18,13 @@ import {BreadcrumbModule} from 'xng-breadcrumb';
     CommonModule,
     RouterModule,
     BreadcrumbModule,
+    SharedModule,
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
       preventDuplicates: true
     })
   ],
-  exports : [
+  exports: [
     NavBarComponent,
     SectionHeaderComponent
   ]
