6d982a4f18e20b4b11804346170898ffe1e1950f
Boli-0603 <bli0603@gmail.com>
2021-06-21 11:36:12 +1200

    End of section 21 part 1

--- client/src/app/shared/models/basket.ts
+++ client/src/app/shared/models/basket.ts
@@ -3,6 +3,10 @@ import { v4 as uuidv4 } from 'uuid';
 export interface IBasket {
     id: string;
     items: IBasketItem[];
+    clientSecret?: string;
+    paymentIntentId?: string;
+    deliveryMethodId?: number;
+    shippingPrice?: number;
 }
 
 export interface IBasketItem {
@@ -25,3 +29,4 @@ export interface IBasketTotals {
     subtotal: number;
     total: number;
 }
+
